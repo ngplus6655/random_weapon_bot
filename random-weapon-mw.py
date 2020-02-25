77170ccc0d1b5bc7e849fadec198f9e7f547df0e
rocoh <rocoh@LAPTOP-464UG7P3.localdomain>
2020-02-26 05:55:28 +0900

    fix comment

--- random-weapon-mw.py
+++ random-weapon-mw.py
@@ -214,13 +214,8 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「??rand」と発言したら「にゃーん」が返る処理
     if message.content == '?loadout':
         # メイン武器取得
-        # name_kwy で名前取得
-        # key(カテゴリー)でlistを作ってraondom.choice
-        # value(アタッチ)でlistを作ってrandom.sample 5
-        # でうまくいくかな？？
         main_category = random.choice( main_l )
         main = main_rand( main_category )
         main_name = main[0]
